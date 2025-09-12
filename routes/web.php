bf4dc4a5a629fe6ad974020284bc1736a9d2fe08
gitcore33 <erickestanislau25@gmail.com>
2025-09-12 01:24:03 -0300

    add first routes

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,25 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/imprensa', function () {
+    return view('imprensa');
+});
+
+Route::get('/merch', function () {
+    return view('merch');
+});
+
+Route::get('/apoie', function () {
+    return view('apoie');
+});
+
+Route::get('/eventos', function () {
+    return view('eventos');
+});
+
+Route::get('/contato', function () {
+    return view('contato');
 });
